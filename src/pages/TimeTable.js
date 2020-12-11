4583376570b02acccba423341edf2a7f8c3ac53c
qkrwoghk <akavmvkdldj1@naver.com>
2020-12-11 22:49:01 +0900

    20201211 20:48 update ver

--- src/pages/TimeTable.js
+++ src/pages/TimeTable.js
@@ -1,10 +1,12 @@
 import React, { Component, useRef,  useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert, FlatList} from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Alert, FlatList, Dimensions} from 'react-native';
 import { Table, TableWrapper,Col, Cols, Cell, Row, Rows } from 'react-native-table-component';
-import BottomSheet from "react-native-swipeable-bottom-sheet"
 import RBSheet from "react-native-raw-bottom-sheet";
 import { SearchBar } from 'react-native-elements';
+import Popover, { PopoverMode, PopoverPlacement } from 'react-native-popover-view';
+import PopMap from '../components/PopMap';
 
+const {height: SCREEN_HEIGHT, width: SCREEN_WIDTH} = Dimensions.get('window');
 ////////////////////////////////////////////////// Modal Screen //////////////////////////////////////////////////
 const SearchList = () =>{
   const [search, setSearch] = useState('');
@@ -22,6 +24,32 @@ const SearchList = () =>{
         console.error(error);
       });
   }, []);
+  
+  function InfoButton(props) {
+    const touchable = useRef();
+    const [showPopover, setShowPopover] = useState(false);
+   
+    return (
+      <>
+        <TouchableOpacity ref={touchable} onPress={() => setShowPopover(true)} style={{flex: 1, marginRight: 20}}>
+          <View style={modalStyles.btn}>
+            <Text style={modalStyles.btnText}>info</Text>
+          </View>
+        </TouchableOpacity>
+        <Popover 
+          from={touchable} 
+          isVisible={showPopover}
+          placement = 'auto'
+          onRequestClose={() => setShowPopover(false)}
+          >
+            <View style={modalStyles.popov}>
+              <Text>{props.content}</Text>
+              <PopMap lat={props.id/10000} long ={props.id/10000}></PopMap>
+            </View>
+        </Popover>
+      </>
+    );
+  }
 
   const searchFilterFunction = (text) => {
     // Check if searched text is not blank
@@ -49,11 +77,14 @@ const SearchList = () =>{
   const ItemView = ({ item }) => {
     return (
       // Flat List Item
-      <Text style={searchstyles.itemStyle} onPress={() => getItem(item)}>
-        {item.id}
-        {'.'}
-        {item.title.toUpperCase()}
-      </Text>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <Text style={modalStyles.itemStyle} onPress={() => getItem(item)}>
+          {item.id}
+          {'.'}
+          {item.title.toUpperCase()}
+        </Text>
+        <InfoButton id={item.id} content={item.title}></InfoButton>
+      </View>
     );
   };
 
@@ -72,18 +103,19 @@ const SearchList = () =>{
 
   const getItem = (item) => {
     // Function for click on an item
-    alert('Id : ' + item.id + ' Title : ' + item.title);
+  
+    Alert.alert('Id : ' + item.id + ' Title : ' + item.title + '를 선택하시겠습니까?');
   };
 
   return (
     <View style={{ flex: 1 }}>
-      <View style={searchstyles.container}>
+      <View style={modalStyles.container}>
         <SearchBar
           round
           searchIcon={{ size: 24 }}
           onChangeText={(text) => searchFilterFunction(text)}
           onClear={(text) => searchFilterFunction('')}
-          placeholder="Type Here..."
+          placeholder="과목 검색..."
           value={search}
         />
         <FlatList
@@ -97,13 +129,17 @@ const SearchList = () =>{
   );
 };
 
-const searchstyles = StyleSheet.create({
+const modalStyles = StyleSheet.create({
   container: {
     backgroundColor: '#A6A6A6',
   },
   itemStyle: {
+    flex: 9,
     padding: 10,
   },
+  btn: { width: 50, height: 18, backgroundColor: '#78B7BB',  borderRadius: 20 },
+  btnText: { textAlign: 'center', color: '#fff' },
+  popov: { width: SCREEN_WIDTH*4/5, height: SCREEN_HEIGHT/3 , padding: 5}
 });
 ////////////////////////////////////////////////// Modal Screen //////////////////////////////////////////////////
 
@@ -126,7 +162,7 @@ const BottomSwipeButton = () => {
       <RBSheet
             ref = {modalizeRef}
             height = {780}
-            animationType = "fade"
+            animationType = "slide"
             closeOnDragDown = {true}
             closeOnPressMask = {true}
             dragFromTopOnly = {true}
@@ -164,16 +200,17 @@ export default class Navigate extends Component {
  
     this.state = {
       tableTime: [['9:00-'], ['10:30-'], ['12:00-'], ['13:30-'], ['15:00-'], ['16:30-'], ['18:00-']],
-      tableHead: [elementButton('Mon'), elementButton('Tue'), elementButton('Wed'), elementButton('Thu'), elementButton('Fri')],
+      tableHead: [elementButton('월'), elementButton('화'), elementButton('수'), elementButton('목'), elementButton('금')],
     }
   }
  
   _alertIndex(value) {
-    Alert.alert(`This is column ${value}`);
+    Alert.alert(`${value}요일`);
   }
   
   render() {
     const state = this.state;
+
     const tableData = [];
     for (let i = 0; i < 7; i += 1) {
       const rowData = [];
@@ -190,7 +227,7 @@ export default class Navigate extends Component {
             <TableWrapper style={{width: 80}}>
               <Cell data="" style={styles.singleHead}/>
               <TableWrapper style={{flexDirection: 'row'}}>
-                <Col data={['AM', 'PM']} style={styles.head} heightArr={[80, 200]} textStyle={styles.btnText} />
+                <Col data={['오전', '오후']} style={styles.head} heightArr={[80, 200]} textStyle={styles.btnText} />
                 <Col data={state.tableTime} style={[styles.head]} heightArr={[40, 40, 40, 40, 40, 40, 40, 40]} textStyle={styles.timeText}></Col>
               </TableWrapper>
             </TableWrapper>
@@ -234,7 +271,8 @@ const styles = StyleSheet.create({
   title: { backgroundColor: '#737373' },
   timeText: { marginRight: 6, textAlign:'right', fontSize: 10 },
   text: { textAlign: 'center'},
-  btn: { height: 58, backgroundColor: '#737373', borderRadius: 2 },
+
+  btn: { height: 58, backgroundColor: '#A6A6A6', borderRadius: 2 , justifyContent: 'center'},
   btnText: { textAlign: 'center', fontSize: 25, fontWeight: '600', fontFamily: "Cochin" },
   row: {backgroundColor: '#737373' },
 

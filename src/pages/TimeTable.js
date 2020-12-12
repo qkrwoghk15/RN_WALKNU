23c04c1d66a19163003d0b6910d40a610de0ac86
qkrwoghk <akavmvkdldj1@naver.com>
2020-12-12 19:26:49 +0900

    update 20201212 19:26 ver

--- src/pages/TimeTable.js
+++ src/pages/TimeTable.js
@@ -7,51 +7,32 @@ import Popover, { PopoverMode, PopoverPlacement } from 'react-native-popover-vie
 import Modal from 'react-native-modal';
 
 import PopMap from '../components/PopMap';
+import convertToTime from '../components/functions'
 
+var createReactClass = require('create-react-class');
 const {height: SCREEN_HEIGHT, width: SCREEN_WIDTH} = Dimensions.get('window');
+var enrollArr=['알고리즘', '데이터베이스'], 
+  enrollTimeArr=[
+    {day: 0, start: 0, end: 3},
+    {day: 1, start: 4, end: 6},
+  ];
 ////////////////////////////////////////////////// Modal Screen //////////////////////////////////////////////////
-const SearchList = () =>{
+const SearchList = (props) =>{
   const [search, setSearch] = useState('');
   const [filteredDataSource, setFilteredDataSource] = useState([]);
   const [masterDataSource, setMasterDataSource] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('http://3.218.74.114/graphql?query={ getLectures(str: "컴퓨터") { cid, univ, cname, prof, ltime } }')
       .then((response) => response.json())
       .then((responseJson) => {
-        setFilteredDataSource(responseJson);
-        setMasterDataSource(responseJson);
+        setFilteredDataSource(responseJson.data.getLectures);
+        setMasterDataSource(responseJson.data.getLectures);
       })
       .catch((error) => {
         console.error(error);
       });
-  }, []);
-  
-  function InfoButton(props) {
-    const touchable = useRef();
-    const [showPopover, setShowPopover] = useState(false);
-   
-    return (
-      <>
-        <TouchableOpacity ref={touchable} onPress={() => setShowPopover(true)} style={{flex: 1, marginRight: 20}}>
-          <View style={modalStyles.btn}>
-            <Text style={modalStyles.btnText}>info</Text>
-          </View>
-        </TouchableOpacity>
-        <Popover 
-          from={touchable} 
-          isVisible={showPopover}
-          placement = 'auto'
-          onRequestClose={() => setShowPopover(false)}
-          >
-            <View style={modalStyles.popov}>
-              <Text>{props.content}</Text>
-              <PopMap lat={props.id/10000} long ={props.id/10000}></PopMap>
-            </View>
-        </Popover>
-      </>
-    );
-  }
+  }, []);  
 
   const searchFilterFunction = (text) => {
     // Check if searched text is not blank
@@ -60,8 +41,8 @@ const SearchList = () =>{
       // Filter the masterDataSource
       // Update FilteredDataSource
       const newData = masterDataSource.filter(function (item) {
-        const itemData = item.title
-          ? item.title.toUpperCase()
+        const itemData = item.cid
+          ? item.cid.toUpperCase()
           : ''.toUpperCase();
         const textData = text.toUpperCase();
         return itemData.indexOf(textData) > -1;
@@ -81,11 +62,13 @@ const SearchList = () =>{
       // Flat List Item
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
         <Text style={modalStyles.itemStyle} onPress={() => getItem(item)}>
-          {item.id}
-          {'.'}
-          {item.title.toUpperCase()}
+          {item.cid}
+          {'\n'}
+          {item.cname.toUpperCase()}
+          {'\n'}
+          {item.prof}
         </Text>
-        <InfoButton id={item.id} content={item.title}></InfoButton>
+        <InfoButton id={item.cid} cname = {item.cname} univ={item.univ}></InfoButton>
       </View>
     );
   };
@@ -103,10 +86,47 @@ const SearchList = () =>{
     );
   };
 
-  const getItem = (item) => {
+  const getItem = (item) =>
     // Function for click on an item
-    Alert.alert('Id : ' + item.id + ' Title : ' + item.title + '를 선택하시겠습니까?');
-  };
+    Alert.alert(
+        `과목명 : ${item.cname}\n시간: ${item.ltime}\n수강하시겠습니까?`,
+        ``,
+        [
+          { text: "OK", onPress: () => {props.modalVisible.current?.close(); } },//enrollArr.push(item)
+          {
+            text: "Cancel",
+            style: "cancel"
+          },
+        ],
+        { cancelable: false }
+      );
+
+  function InfoButton(props) {
+    const touchable = useRef();
+    const [showPopover, setShowPopover] = useState(false);
+   
+    return (
+      <>
+        <TouchableOpacity ref={touchable} onPress={() => setShowPopover(true)} style={{flex: 1, marginRight: 20}}>
+          <View style={modalStyles.btn}>
+            <Text style={modalStyles.btnText}>info</Text>
+          </View>
+        </TouchableOpacity>
+        <Popover 
+          from={touchable} 
+          isVisible={showPopover}
+          placement = 'auto'
+          onRequestClose={() => setShowPopover(false)}
+          >
+            <View style={modalStyles.popov}>
+              <Text>강의명: {props.cname}</Text>
+              <Text>장소: {props.univ}</Text>
+              <PopMap lat={1/10000} long ={1/10000}></PopMap>
+            </View>
+        </Popover>
+      </>
+    );
+  }
 
   return (
     <View style={{ flex: 1,  backgroundColor: '#A6A6A6'}}>
@@ -176,12 +196,47 @@ const BottomSwipeButton = () => {
             }
           }}
         >
-          <SearchList></SearchList>
+          <SearchList modalVisible={modalizeRef}></SearchList>
       </RBSheet>
     </>
   );
 }
 ////////////////////////////////////////////////// Modal Button //////////////////////////////////////////////////
+const Rect=(props) =>{
+  const color =["red","orange","gray","black","blue"];
+  const no=parseInt(Math.random()*5); //random(0.0~0.99)
+  return(
+    <TouchableOpacity 
+      activeOpacity = {1,1}
+      onPress={() => Alert.alert(props.cname)}
+      style={{
+        position: 'absolute',
+        justifyContent: 'center',
+        backgroundColor: color[no],
+        left: 35 + (SCREEN_WIDTH-55)/5*props.day,
+        top: 0 + (40)*props.start,
+        width: (SCREEN_WIDTH-55)/5,
+        height: 40*(props.end-props.start+1)}}>
+      <View>
+          <Text style={styles.subText}>{props.cname}</Text>
+      </View>
+    </TouchableOpacity>
+      
+  )
+}
+
+var EnrollList = createReactClass({ 
+  render: function() { 
+      var lists = enrollTimeArr.map(function(list, index){ 
+          return <Rect day={list.day} start={list.start} end={list.end} cname={enrollArr[index]}></Rect>
+      }.bind(this));
+       
+      return (
+            <>{lists}</>
+          ); 
+      } 
+  }); 
+
 class ElementButton extends Component{
   constructor(props){
     super();
@@ -191,23 +246,23 @@ class ElementButton extends Component{
     }
   }
 
-  toggleModal = () => {
-    this.setState({isModalVisible: !this.state.isModalVisible});
+  toggleModal = (visible) => {
+    this.setState({isModalVisible: !visible});
   }
 
   render(){
     return(
       <>
-        <TouchableOpacity onPress={() => this.toggleModal}>
+        <TouchableOpacity onPress={() => this.toggleModal(this.state.isModalVisible)}>
           <View style={styles.btn}>
             <Text style={styles.btnText}>{this.state.value}</Text>
           </View>
         </TouchableOpacity>
 
         <Modal isVisible={this.state.isModalVisible}>
-          <View>
-            <Text>요일</Text>
-            <Button title="Hide modal" onPress={this.toggleModal} />
+          <View style={{alignItems: 'center'}}>
+              <Text>요일</Text>
+              <Button title="Hide modal" onPress={this.toggleModal} />
           </View>
         </Modal>
       </>
@@ -221,57 +276,62 @@ export default class TimeTable extends Component {
 
     this.state = {
       time: 8,
+      heightAry: [],
       tableTime: [],
       tableHead: [<ElementButton value='월'/>, <ElementButton value='화'/>, <ElementButton value='수'/>, <ElementButton value='목'/>, <ElementButton value='금'/>],
     }
   }
-  
+
   render() {
     const state = this.state;
-    for(let i=0; i<28; i+=1){
+
+    for(let i=0; i<27; i+=1){
         this.state.tableTime.push((state.time<10? `0${state.time}` : `${state.time}`) + (!(i%2)? ":00" : ":30"))
         if(i%2) state.time += 1
+        state.heightAry.push(40)
     }
 
     const tableData = [];
     for (let i = 0; i < 5; i += 1) {
       const colData = [];
-      colData.push(state.tableHead[i]);
-      for (let j = 0; j < 7; j += 1) {
-        colData.push(`${i}${j}`);
+      for (let j = 0; j < 27; j += 1) {
+        colData.push(``);
       }
       tableData.push(colData);
     }
 
     return (
       <View style={styles.container}>
-        <ScrollView vertical={true}>
-          <View style={{flex: 9, padding: 10, paddingTop: 30, flexDirection: 'row', alignItems:'flex-start'}}>
-            <Table style={{flexDirection: 'row'}} borderStyle={{borderWidth: 1}}>
-              <TableWrapper style={{width: 40}}>
-                <Cell data="" style={styles.singleHead}/>
-                <TableWrapper style={{flexDirection: 'row'}}>
-                  <Col data={state.tableTime} style={[styles.head]} heightArr={[60, 60, 60, 60, 60, 60, 60, 60]} textStyle={styles.timeText}></Col>
+        <View style={{flex: 7, padding: 10, paddingTop: 30}}>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <TableWrapper style={{flexDirection: 'row'}}>
+                <Cell data="" style={{width: 35, backgroundColor: '#A6A6A6'}}/>
+                <TableWrapper style={{flex: 1}}>
+                  <Row data={state.tableHead} flex={1} style={styles.header} textStyle={styles.text}/>
                 </TableWrapper>
               </TableWrapper>
+            </Table>
 
-              <TableWrapper style={{flex:1, flexDirection: 'row', backgroundColor: '#404040'}}>
+            <ScrollView bounces={false}>
+              <Table style={{flexDirection: 'row'}} borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                <TableWrapper style={{width: 35}}>
+                    <Col data={state.tableTime} style={[styles.head]} heightArr={state.heightAry} textStyle={styles.timeText}></Col>
+                </TableWrapper>
                 {
                   tableData.map((colData, index) => (
                     <Col
                       key={index}
                       data={colData}
-                      flex={1}
-                      height={60}
+                      heightArr={state.heightAry}
                       style={[styles.row]}
                       textStyle={styles.text}
                     />
                   ))
                 }
-              </TableWrapper>
-            </Table>
-          </View>
-        </ScrollView>
+              </Table>
+              <EnrollList {...this.props}></EnrollList>
+            </ScrollView>
+        </View>
 
         <View style={{flex: 1, flexDirection:'column-reverse', paddingBottom: 50}}>
           <BottomSwipeButton></BottomSwipeButton>
@@ -289,10 +349,9 @@ const styles = StyleSheet.create({
     padding: 0,
     backgroundColor: '#404040',
   },
-  singleHead: { width: 40, height: 60, backgroundColor: '#A6A6A6' },
-  head: { backgroundColor: '#A6A6A6' },
-  title: { backgroundColor: '#737373' },
-  timeText: { marginRight: 6, textAlign:'center', fontSize: 8, flexWrap: 'nowrap' },
+  head: { flex: 1, backgroundColor: '#A6A6A6', alignItems: 'stretch'},
+  title: { flex: 1, backgroundColor: '#737373' },
+  timeText: { flex: 1, textAlign:'right', fontSize: 8, flexWrap: 'nowrap'},
   text: { textAlign: 'center'},
 
   btn: { height: 58, backgroundColor: '#A6A6A6', borderRadius: 2 , justifyContent: 'center'},
@@ -313,4 +372,11 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
   },
+  subText: {
+    fontSize: 10,
+    color: 'white',
+    flexWrap: 'nowrap',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  }
 });
